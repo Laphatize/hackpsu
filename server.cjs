6d33c7213495e2cc7e14fc1c131ce0a5719ac831
Pranav Ramesh <laphatize@protonmail.com>
2022-11-05 16:10:40 -0400

    br

--- server.cjs
+++ server.cjs
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
-
-
-app.get('/', (req, res) => {
+// Base Endpoint
+app.post('/', (req, res) => {
   res.send('hello world')
 })
+
+
 
 
 
